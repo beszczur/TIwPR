beb938af1d1d1ef9a3180adc68823c224fb9d0c5
beszczur <beszczur@gmail.com>
2017-05-20 21:07:30 +0200

    backend - part 2

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import tornado.web
 import sqlite3 as sqlite
 from tornado.escape import json_encode
 
+
 # /events                   (GET, POST)
 # /events/{eid}             (GET, PUT, DELETE)
 # /events/{eid}/tasks       (GET, POST)
@@ -20,6 +21,7 @@ class Event:
         self.date = date
         self.repeatOptions = repeatId
 
+
 class Task:
     def __init__(self, id, eventId, name, status, priority):
         self.id = id
@@ -28,6 +30,7 @@ class Task:
         self.status = status
         self.priority = priority
 
+
 ########################################### DATABASE #############################################
 def dict_factory(cursor, row):
     d = {}
@@ -35,9 +38,11 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
+
 conn = sqlite.connect('todo.db')
 conn.row_factory = dict_factory
 c = conn.cursor()
+
 
 def addEvent(name, position, date, repeatId):
     c.execute('INSERT INTO events VALUES (NULL,?,?,?,?)', (name, position, date, repeatId))
@@ -53,10 +58,20 @@ def eventKeysExists(data):
     return True
 
 
+def isEventExist(id):
+    c.execute('SELECT count(*) from events where id=' + id)
+    exists = c.fetchone()['count(*)']
+    if exists == 0:
+        return False
+    else:
+        return True
+
+
 ######################################## REST ####################################################
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write({ 'TO DO' : 'LIST' })
+        self.write({'TO DO': 'LIST'})
+
 
 # class MainHandler(tornado.web.RequestHandler):
 #     def get(self):
@@ -76,29 +91,45 @@ class MainHandler(tornado.web.RequestHandler):
 #         self.write("OK")
 
 class EventsCollectionHandler(tornado.web.RequestHandler):
-    def get(self): #DONE
+    def get(self):  # DONE
         c.execute('SELECT * from events')
         result_set = c.fetchall()
         self.write(json.dumps(result_set))
 
-    def post(self): #DONE
+    def post(self):  # DONE
         data = json.loads(self.request.body.decode('utf-8'))
         if eventKeysExists(data):
-            self.write({ 'Event added, id:' : addEvent(data['name'], data['position'], data['date'], data['repeatId']) })
+            # Should it be 201 - Created ?
+            self.write({'Event added, id:': addEvent(data['name'], data['position'], data['date'], data['repeatId'])})
         else:
             print "Missing data in request"
-            self.write({ 'Missing data' : 'in your request' })
+            self.write({'Missing data': 'in your request'})
+
 
 class EventHandler(tornado.web.RequestHandler):
-    def get(self,id): #DONE
-        c.execute('SELECT * from events where id='+id)
+    def get(self, id):  # DONE
+        c.execute('SELECT * from events where id=' + id)
         result_set = c.fetchall()
         self.write(json.dumps(result_set))
+        # Should I return 404 if id doesn't exist in database ?
+
+    def put(self, id):
+        print "PUT"
+
+    def delete(self, id): # DONE
+        if isEventExist(id):
+            c.execute('DELETE FROM events WHERE id=?', (id,))
+            conn.commit()
+            # Task which refers to this event should also be deleted
+            self.write({'DELETED': '#which code here ? 200 ?'})
+        else:
+            self.write({'Event dosen\'t exist': id})
+
 
 if __name__ == "__main__":
     application = tornado.web.Application([
         ("/", MainHandler),
-        ("/events", EventsCollectionHandler), #DONE
+        ("/events", EventsCollectionHandler),  # DONE
         ("/events/([0-9]+)", EventHandler)
     ])
     application.listen(8888)
